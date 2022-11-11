83767370dbb2abcbe9d8416c3e4c80a248588312
KateRitchie <ritchie_ecen@hotmail.com>
2022-11-10 19:47:01 -0700

    testing OpenWeather API

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 //Open Weather API Key
 var APIKey = "4b15e9c838f839b2de2a4b338f140576";
-var city;
+var city = document.getElementById('searchCity');
+var testCity = "denver"
+var callTestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + testCity + "&appid=" + APIKey;
+
+function getCityweather() {
+    fetch(callTestURL)
+    .then(function (reponse) {
+        return reponse.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        var coord = data.coord;
+        })
+}
+
+function forecast(coord) {
+    var lat = coord.lat
+    var lon = coord.lon
+    
+}
+
+getCityweather();
